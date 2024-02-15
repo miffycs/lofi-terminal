beecace61f083f925314ce80689dd808c8b6da8b
Miffy Chen <mchen15@bu.edu>
2024-02-14 21:50:48 -0800

    update terminal UI

--- components/terminal/terminal.jsx
+++ components/terminal/terminal.jsx
@@ -10,6 +10,17 @@ const commands = {
   prevsong: () => console.log("previous song"),
   play: () => console.log("play"),
   pause: () => console.log("pause"),
+  about: (
+    <span>
+      Welcome to Lofi Terminal!
+      <br />
+      <br />
+      Created by <a href="https://miffy.io">Miffy</a>
+      <br />
+      Created with{" "}
+      <a href="https://github.com/bony2023/react-terminal">react-terminal</a>
+    </span>
+  ),
   help: (
     <span>
       hello - say hello <br />
@@ -18,11 +29,21 @@ const commands = {
       prevsong - play previous song <br />
       play - play song <br />
       pause - pause song <br />
+      about - show about message <br />
       help - show help message <br />
       clear - clear screen
     </span>
   ),
 };
+
+const welcomeMessage = (
+  <span>
+    Welcome to{" "}
+    <a href="https://github.com/miffycs/lofi-terminal">Lofi Terminal</a>, type
+    &quot;help&quot; for all available commands
+    <br />
+  </span>
+);
 
 export default function Terminal() {
   return (
